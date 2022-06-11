0c4ef000c1959516ae68dcf57b69d1ec066d93a3
Boondock19 <josematiasgonzalez20@gmail.com>
2022-06-11 02:36:38 -0400

    Arreglado metodo string de los cuaternarios

--- Examen1/4/cuaternarios.py
+++ Examen1/4/cuaternarios.py
@@ -9,12 +9,14 @@ class Cuaternario:
         self.c = c
         self.d = d
 
+    # Sobrecarga de operador suma
     def __add__(self,o):
         if (type(o) is int or type(o) is float) :
             return Cuaternario((self.a + o), (self.b), (self.c) , (self.d))
         else:
             return Cuaternario((self.a + o.a), (self.b + o.b), (self.c + o.c) , (self.d + o.d))
 
+    # Sobrecarga de operador mult
     def __mul__(self,o):
         if (type(o) is int or type(o) is float) :
             return Cuaternario((self.a * o), (self.b), (self.c) , (self.d))
@@ -24,7 +26,7 @@ class Cuaternario:
         D = self.a*o.d + self.b*o.c - self.c*o.b + self.d*o.a 
         return Cuaternario(A, B, C , D)
 
-    # ~
+    #  Sobrecarga de operador suma ~
     def __invert__(self):
         B = -self.b
         C = -self.c
@@ -42,4 +44,9 @@ class Cuaternario:
         return square
 
     def __str__(self):
-        return "{} , {}i ,  {}j ,  {}k".format(self.a,self.b,self.c,self.d)
+        stringA = str(self.a)
+        stringB = " + " + str(self.b) + "i" if self.b > 0 else  " " + str(self.b) + "i"
+        stringC = " + " + str(self.c) + "j" if self.c > 0 else  " " + str(self.c) + "j"
+        stringD = " + " + str(self.d) + "k" if self.d > 0 else  " " + str(self.d) + "k"
+
+        return stringA + stringB + stringC + stringD 
