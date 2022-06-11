df91a626566aa67c3022cf4472206ef48ae12904
Boondock19 <josematiasgonzalez20@gmail.com>
2022-06-11 01:56:00 -0400

    Realizada parte b de la 4

---
+++ Examen1/4/cuaternarios.py
@@ -0,0 +1,40 @@
+
+from math import sqrt
+
+
+class Cuaternario:
+    def __init__(self,a,b,c,d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def __add__(self,o):
+        return Cuaternario((self.a + o.a), (self.b + o.b), (self.c + o.c) , (self.d + o.d))
+
+    def __mul__(self,o):
+        A = self.a*o.a - (self.b*o.b) - (self.c*o.c) - (self.d*o.d)
+        B = self.a*o.b + self.b*o.a + self.c*o.d - self.d*o.c
+        C = self.a*o.c - self.b*o.d + self.c*o.a + self.d*o.b
+        D = self.a*o.d + self.b*o.c - self.c*o.b + self.d*o.a 
+        return Cuaternario(A, B, C , D)
+
+    # ~
+    def __invert__(self):
+        B = -self.b
+        C = -self.c
+        D = -self.d
+        return Cuaternario(self.a,B,C,D)
+
+     # & , debemos cambiarlo por +(pos) porque no tenemos operador & unario en python
+     # y no nos permiten cambiar la cantidad de binario a unario y viceversa.
+    def __pos__(self):
+        A = self.a**2
+        B = self.b**2
+        C = self.c**2
+        D = self.d**2
+        square = sqrt(A+B+C+D)
+        return square
+
+    def __str__(self):
+        return "{} , {}i ,  {}j ,  {}k".format(self.a,self.b,self.c,self.d)
---
+++ Examen1/4/main.py
@@ -0,0 +1,60 @@
+from cuaternarios import Cuaternario
+
+
+print("Por favor ingresa el valor del cuarternario (a,b,c,d) y decimales separados por . (Ejemplo 25.4)")
+print("Ingresalos separados por un espacio en blanco , a b c d  ")
+strCuaternario1 = input("Primer cuaternario ")
+
+strArray1 = strCuaternario1.split(" ")
+
+
+cuar = Cuaternario(float(strArray1[0]),float(strArray1[1]),float(strArray1[2]),float(strArray1[3]))
+print(cuar)
+
+strCuaternario2 = input("Segundo cuaternario ")
+
+strArray2 = strCuaternario2.split(" ")
+
+
+cuar2 = Cuaternario(float(strArray2[0]),float(strArray2[1]),float(strArray2[2]),float(strArray2[3]))
+print(cuar2)
+
+cuar3 = cuar + cuar2
+cuar3Negate = ~cuar3
+
+print("Cuaternario resultante de la suma :  \n", cuar3)
+
+print("Cuaternario resultante de la conjugada de {}  es {} \n".format(cuar3,cuar3Negate))
+
+cuar4 = Cuaternario(0.0,1.0,2.0,3.0)
+cuar4Cuadrado = cuar4*cuar4
+modulo = +cuar4Cuadrado
+
+print('El doble de {0} es :   {1} \n'.format(cuar4,cuar4Cuadrado))
+print('El modulo de {0} es :   {1} \n'.format(cuar4Cuadrado,modulo))
+
+print("Probando operaciones : ")
+
+print("Multiplicacion {0} * {1} + {2}  \n".format(cuar2,cuar3,cuar))
+mult = cuar2 * cuar3
+print("Resultado de solo mult  " , mult)
+suma = cuar2 * cuar3 + cuar
+print("Resultado :  ",suma)
+
+print("(b+b) * (c + ~a) \n")
+
+print("Resultado primera suma , ",cuar2 + cuar3)
+
+print("Resultado segunda suma con negado , ",(cuar + ~cuar3))
+
+resul = (cuar2 + cuar3) * (cuar + ~cuar3)
+
+print("Resultado :   ",resul)
+
+print("MODULO de un producto : ")
+
+print("Multiplicacion : ", cuar2 * cuar3)
+
+print("Modulo = : ", +(cuar2 * cuar3))
+
+print("Confirmacion Modulo : ", +Cuaternario(-36.0,6.0,12.0,12.0))
